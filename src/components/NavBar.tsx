6b5726c7fab9db2b7b18dbd163ea0ad6bf4e9544
jkhopkins39 <jeremyyhop@gmail.com>
2025-04-26 19:16:12 -0400

    navbar update

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,32 @@
-import { Link } from "react-router-dom"; // if using React Router
 import React from "react";
 
-const Navbar: React.FC = () => {
+const NavBar = () => {
   return (
-    <nav className="fixed top-0 left-0 w-full h-16 bg-white shadow-md flex items-center justify-between px-4">
-      <div className="text-xl font-bold">MySite</div>
-      <div className="space-x-4">
-        <a href="/">Home</a>
-        <a href="/projects">Projects</a>
-        <a href="/contact">Contact</a>
-      </div>
+    <nav className="bg-blue-500 p-4">
+      <ul className="flex space-x-6">
+        <li>
+          <a href="#home" className="text-white hover:text-gray-200">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="#about" className="text-white hover:text-gray-200">
+            About
+          </a>
+        </li>
+        <li>
+          <a href="#services" className="text-white hover:text-gray-200">
+            Services
+          </a>
+        </li>
+        <li>
+          <a href="#contact" className="text-white hover:text-gray-200">
+            Contact
+          </a>
+        </li>
+      </ul>
     </nav>
   );
 };
 
-export default Navbar;
+export default NavBar;
